7028739340b2c3087b0702da5eccfa85d62d99e6
Allen Shi <allen.sgn@gmail.com>
2017-11-13 14:44:53 -0800

    Add files via upload

---
+++ clustering.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import numpy as np
+from sklearn.cluster import AgglomerativeClustering
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+
+
+df = pd.read_pickle('Two_sigma')
+df = df.reset_index()
+#train
+df1 = df[0:30000]
+#predict
+df2 = df[30000::]
+
+k = AgglomerativeClustering(n_clusters=5)
+dis = k.fit(np.array(df1[['latitude', 'longitude']]))
+df1.loc[::, 'class']=k.labels_
+
+print ('HAHAHAHAHAHAHAHAHA')
+
+qda = QuadraticDiscriminantAnalysis()
+
+qda.fit(np.array(df1[['latitude', 'longitude']]), k.labels_,  store_covariances=None)
+df2.loc[::, 'class']=qda.predict(np.array(df2[['latitude', 'longitude']]))
+
+frames = [df1, df2]
+result = pd.concat(frames)
+
+result.to_pickle('New')
